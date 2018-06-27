008147e121470a81e70a3ce0219fbddeb37de135
Mizuki Tanaka <tanaka.mizuki1029@gmail.com>
2018-06-27 13:44:36 +0900

    add commentout

--- tsp_ga_16.java
+++ tsp_ga_16.java
@@ -1,50 +1,54 @@
+
 /*
  * This program follows the steps below:
  * 
  * 1. read lines from csv file and store the coordinates(x, y)
- * 2. create routes((n-1)! versions)
- * 3. calculate each total distance
- * 4. sort route(quick sort)
- * 5. make random route
- * 6. make random multiple route
- * 7. choice parent route
- * 8. make next generations
+ * 2. make random multiple route 
+ * 3. calculate each total distance of all routes
+ * 4. sort route in distance order(quick sort)
+ * 5. copy Top 50(int parent) routes into parent routes
+ * 6. mutation (copy parent route into routes[1]~[10] & swap its order randomly) *
+ * 7. make multiple combined parent routes[11]~[N-1]
+ * 8. sort routes in distance order
+ * 9. repeat step5 ~ step8 10000 times 
+ * 10. write best route to csv file and display the best distance
  * */
 import java.io.*;
 import java.util.*;
 
-public class tsp_ga_16 {
+public class tsp_ga_16{
 	final static int N = 16;
-	final static int M = 500; //first choice the number of random routes
-	final static double coordinates[][] = new double[N][2];//the coordinates(x, y)
-	final static int parent = 50; //the number of parent routes
+	final static int M = 500; // first choice the number of random routes
+	final static double coordinates[][] = new double[N][2];// the coordinates(x,y)
+	final static int parent = 50; // the number of parent routes
 	static int bestRoute[] = new int[N];
 	static double allDistances[] = new double[M];
 	static double bestDistance;
 	static int routes[][] = new int[M][N];
 	static int parentRoutes[][] = new int[parent][N];
 	static int routeNum = 0;
-	final static int mutation = 10; //the number of mutation
+	final static int mutation = 10; // the number of mutation突然変異
+
 	public static void main(String[] args) {
 		readFile("input_2.csv");
-		makeMultipleRoutes(routes);
-		allRoutesDistance(routes);
-        sortRoute(allDistances, 0, M-1);
-        
-        System.out.println("10000th Generations's best route");
-        howGenerations(10000);
-        
-        for(int i = 0; i < routes[0].length; i++){
-            System.out.print(routes[0][i] + " ");
-        }
-        System.out.println(" : " + allDistances[0]);
-        for(int i = 0; i < routes[0].length; i++){
-            bestRoute[i] = routes[0][i];
-        }
-        writeFile("output_2.csv", bestRoute);
+		makeMultipleRoutes(routes);//create 500 random routes
+		allRoutesDistance(routes);/* 3. calculate each total distance*/
+		sortRoute(allDistances, 0, M - 1);/*4. sort route*/
+
+		System.out.println("10000th Generations's best route");
+		howGenerations(10000);
+
+		for (int i = 0; i < routes[0].length; i++) {
+			System.out.print(routes[0][i] + " ");//elite
+		}
+		System.out.println("");
+		System.out.println("best distance : " + allDistances[0]);
+		for (int i = 0; i < routes[0].length; i++) {
+			bestRoute[i] = routes[0][i];//elite
+		}
+		writeFile("output_2.csv", bestRoute);
 	}
 
-	
 	/* 1. read lines from csv file and store the coordinates(x, y) */
 	public static void readFile(String fileName) {
 		try {
@@ -72,211 +76,201 @@ public class tsp_ga_16 {
 			e.printStackTrace();
 		}
 	}
+	/* this method make random route */
+	public static void makeRoute(int route[]) {
+		/* make array according to turn */
+		for (int i = 0; i < route.length; i++) {
+			route[i] = i;
+		}
+		for (int i = 0; i < route.length; ++i) {
+			/* shuffle array */
+			int rnd = (int) (Math.random() * (double) route.length);
+			int copy = route[i];
+			route[i] = route[rnd];
+			route[rnd] = copy;
+		}
+	}
 
-	/*2. this method return the distance between the two points*/
+	/* 2. make random multiple route */
+	public static void makeMultipleRoutes(int routes[][]) {
+		for (int i = 0; i < routes.length; i++) {
+			/* do an i line at random */
+			makeRoute(routes[i]);
+		}
+	}
+
+	
+	/* this method return the distance between the two points */
 	public static double calculateDistance(int start, int goal) {
 		double distance = Math.pow(coordinates[goal][0] - coordinates[start][0], 2)
 				+ Math.pow(coordinates[goal][1] - coordinates[start][1], 2);
 		return Math.sqrt(distance);
 
 	}
-	
-	/*3. calculate each total distance */
+
+	/* this method calculate each total distance */
 	public static double sumDistance(int route[]) {
 		double sumDistance = 0;
-		for (int i = 0; i < route.length-1; i++) {
-			sumDistance += calculateDistance(route[i], route[i+1]);
+		for (int i = 0; i < route.length - 1; i++) {
+			sumDistance += calculateDistance(route[i], route[i + 1]);
 		}
-		sumDistance += calculateDistance(route[0], route[route.length-1]);
+		sumDistance += calculateDistance(route[0], route[route.length - 1]);
 		return sumDistance;
 	}
-	
-	/* calcurate all routes's daistance */
-	public static void allRoutesDistance(int routes[][]){
-		for(int i = 0; i < routes.length; i++){
+
+	/* 3. calculate each total distance of all routes */
+	public static void allRoutesDistance(int routes[][]) {
+		for (int i = 0; i < routes.length; i++) {
 			allDistances[i] = sumDistance(routes[i]);
 		}
 	}
 
-	/* 4. sort routes */
+	/* 4. sort random routes in ascending order*/
 	static void sortRoute(double distances[], int left, int right) {
-        if (left >= right) {
-            return;
-        }
-        double p = distances[(left + right) / 2];
+		if (left >= right) {
+			return;
+		}
+		double p = distances[(left + right) / 2];
 		int pivot = 0;
-        int l = left;
+		int l = left;
 		int r = right;
 		double tmp;
-        while(l<=r) {
-            while(distances[l] < p){
+		while (l <= r) {
+			while (distances[l] < p) {
 				l++;
 			}
-            while(distances[r] > p){
+			while (distances[r] > p) {
 				r--;
 			}
-            if (l<=r) {
-                tmp = distances[l]; 
-				distances[l] = distances[r]; 
+			if (l <= r) {
+				tmp = distances[l];
+				distances[l] = distances[r];
 				distances[r] = tmp;
-				for(int i = 0; i < routes[i].length; i++){
+				for (int i = 0; i < routes[i].length; i++) {
 					pivot = routes[l][i];
 					routes[l][i] = routes[r][i];
 					routes[r][i] = pivot;
 				}
-                l++; 
+				l++;
 				r--;
-            }
-        }
-        sortRoute(distances, left, r);  
-        sortRoute(distances, l, right); 
-    }
-	
-
-	/*5. make random route */
-	public static void makeRoute(int route[]){
-        /* make array according to turn */
-		for(int i = 0; i < route.length; i++){
-			route[i] = i;
+			}
 		}
-        for ( int i = 0; i < route.length; ++ i ) {
-            /* shuffle array */
-            int rnd = (int)(Math.random() * (double)route.length);
-    	    int copy = route[i];
-   		    route[i] = route[rnd];
-   		    route[rnd] = copy;
-    	}
+		sortRoute(distances, left, r);
+		sortRoute(distances, l, right);
 	}
 
-	/*6. make random multiple route */
-    public static void makeMultipleRoutes(int routes[][]){
-        for(int i = 0; i < routes.length; i++){
-			/* do an i line at random */
-            makeRoute(routes[i]);
-        }
-    }
-
-	/*7. choice parent route */
-	public static void parentRoutes(){
-		for(int i = 0; i < parentRoutes.length; i++){
-			for(int j = 0; j < parentRoutes[i].length; j++){
+	/* 5. choice parent route */
+	public static void parentRoutes() {
+		for (int i = 0; i < parentRoutes.length; i++) {
+			for (int j = 0; j < parentRoutes[i].length; j++) {
 				parentRoutes[i][j] = routes[i][j];
 			}
 		}
 	}
 
-	/* make mutant route */
-	public static void mutantRoute(){
+	/* 6. mutation (copy parent route into routes[1]~[10] & swap its order randomly) */
+	public static void mutantRoute() {
 		int pivot = 0;
-		
-		for(int i = 0; i < mutation; i++){
-			for(int j = 0; j < routes[i].length; j++){
+		/* copy parent route into routes[1]~[10] */
+		for (int i = 0; i < mutation; i++) {
+			for (int j = 0; j < routes[i].length; j++) {
 				routes[1 + i][j] = parentRoutes[i][j];
 			}
 		}
-		
-		for(int i = 1; i < mutation + 1; i++){
-			int rnd1 = (int)(Math.random() * (double)routes[i].length);
-			int rnd2 = (int)(Math.random() * (double)routes[i].length);
+		/* swap order randomly to routes[1]~[10] */
+		for (int i = 1; i < mutation + 1; i++) {
+			int rnd1 = (int) (Math.random() * (double) routes[i].length);
+			int rnd2 = (int) (Math.random() * (double) routes[i].length);
+			/*swap*/
 			pivot = routes[i][rnd1];
 			routes[i][rnd1] = routes[i][rnd2];
 			routes[i][rnd2] = pivot;
 		}
 	}
 
-	/* combine parent routes */
-	public static void combine(int route[]){
-		
+	/* this method combine parent routes */
+	public static int[] combine(int route[]) {
 		int[][] parentRoutes2 = parentRoutes;
-		int rnd1 = (int)(Math.random() * (double)parentRoutes2.length);
-		int rnd2 = (int)(Math.random() * (double)parentRoutes2.length);
-		int start = (int)(Math.random() * (double)parentRoutes2[0].length);
-		int goal = (int)(Math.random() * (double)parentRoutes2[0].length);
+		int rnd1 = (int) (Math.random() * (double) parentRoutes2.length);
+		int rnd2 = (int) (Math.random() * (double) parentRoutes2.length);
+		int start = (int) (Math.random() * (double) parentRoutes2[0].length);
+		int goal = (int) (Math.random() * (double) parentRoutes2[0].length);
 		int pivot = 0;
 		int flag = 0;
 		int index1 = 0;
 		int index2 = 0;
-		
-		if(start > goal){
+
+		if (start > goal) {//always start < goal
 			pivot = start;
 			start = goal;
 			goal = pivot;
 		}
-		
 		int index3 = goal + 1;
-		
-		for(int i = 0; i < route.length; i++){
+		for (int i = 0; i < route.length; i++) {
 			route[i] = -1;
 		}
-		
-		for(int i = start; i < goal + 1; i++){
+		//select one parent route randomly & change a part of route into the parent route
+		for (int i = start; i < goal + 1; i++) {
 			route[i] = parentRoutes2[rnd1][i];
 		}
 
-		
-		while(index1 < start){
-			
+		while (index1 < start) {//part before start (the changed part)
 			flag = 0;
-			
-			for(int i = 0; i < route.length; i++){
-				if(parentRoutes2[rnd2][index2] == route[i]){
+			for (int i = 0; i < route.length; i++) {
+				if (parentRoutes2[rnd2][index2] == route[i]) {
 					flag = 1;
 				}
 			}
-			
-			if(flag == 0){
+			//if randomly selected city doesn't exist in route[], copy it into route(the part before start)
+			if (flag == 0) {
 				route[index1] = parentRoutes2[rnd2][index2];
 				index1++;
 			}
 			index2++;
-
 		}
-		
-		while(index3 < route.length){
+
+		while (index3 < route.length) {//part after goal (the changed part)
 			flag = 0;
-			
-			for(int i = 0; i < route.length; i++){
-				if(parentRoutes[rnd2][index2] == route[i]){
-					flag++;
+			for (int i = 0; i < route.length; i++) {
+				if (parentRoutes[rnd2][index2] == route[i]) {
+					flag = 1;
 				}
 			}
-			
-			
-			if(flag == 0){
+			//if randomly selected city doesn't exist in route[], copy it into route(the part after goal)
+			if (flag == 0) {
 				route[index3] = parentRoutes[rnd2][index2];
 				index3++;
 			}
 			index2++;
-
 		}
-		
-	}
-	
-	/* make multiple combining parent routes */
-	public static void multipleCombine(){
-		for(int i = mutation + 1; i < routes.length; i++){
-			combine(routes[i]);
-		}
+		return route;
 	}
 
-	/*8. make next generations */
-	public static void nextGeneration(){
+	/* 7. make multiple combined parent routes */
+	public static void multipleCombine() {
+		for (int i = mutation + 1; i < routes.length; i++) {
+			routes[i]=combine(routes[i]);
+		}
+	}
+
+	/* 8. make next generations */
+	public static void nextGeneration() {
 		parentRoutes();
 		mutantRoute();
 		multipleCombine();
 		allRoutesDistance(routes);
-		sortRoute(allDistances, 0, M-1);
-    }
-    
-    /*9. designate number of generations */
-    public static void howGenerations(int number){
-        for(int i = 0; i < number; i++){
-            nextGeneration();
-        }
-    }
+		sortRoute(allDistances, 0, M - 1);
+	}
 
-    /*10. write best route to csv file*/
-    public static void writeFile(String fileName, int bestRoute[]) {
+	/* 9. designate number of generations */
+	public static void howGenerations(int number) {
+		for (int i = 0; i < number; i++) {
+			nextGeneration();
+		}
+	}
+
+	/* 10. write best route to csv file */
+	public static void writeFile(String fileName, int bestRoute[]) {
 		try {
 			File file = new File(fileName);
 			/* exception when the file not exit */
@@ -288,7 +282,7 @@ public class tsp_ga_16 {
 			FileWriter fileWriter = new FileWriter(file);
 			PrintWriter printWriter = new PrintWriter(fileWriter);
 			printWriter.println("index");
-			for(int index = 0; index<bestRoute.length; index++){
+			for (int index = 0; index < bestRoute.length; index++) {
 				printWriter.println(bestRoute[index]);
 			}
 			fileWriter.close();
