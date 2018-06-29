8e9e5d10f86943492347b1a44ee07874b7ec8f76
Mizuki Tanaka <tanaka.mizuki1029@gmail.com>
2018-06-29 10:45:13 +0900

    BF, 3opt, GA

--- src/TSP_GA.java
+++ src/TSP_GA.java
@@ -3,7 +3,7 @@
  * This program follows the steps below:
  * 
  * 1. read lines from csv file and store the coordinates(x, y)
- * 2. make random multiple route 
+ * 2. make random multiple route and sort by 2-opt
  * 3. calculate each total distance of all routes
  * 4. sort route in distance order(quick sort)
  * 5. copy Top 50(int parent) routes into parent routes
@@ -17,37 +17,39 @@ import java.io.*;
 import java.util.*;
 
 public class TSP_GA {
-	final static int N = 128;
+	final static int N = 16;
 	final static int M = 5000; // first choice the number of random routes
 	final static double coordinates[][] = new double[N][2];// the coordinates(x,y)
-	final static int parent = 100; // the number of parent routes
+	final static int parent = 500; // the number of parent routes
 	static int bestRoute[] = new int[N];
 	static double allDistances[] = new double[M];
-	//static double bestDistance;
+	// static double bestDistance;
 	static int routes[][] = new int[M][N];
 	static int parentRoutes[][] = new int[parent][N];
-	static double rate[] = new double[parent];//good parent has high rate
-	//static int routeNum = 0;
-	//final static int mutation = 10; // the number of mutation突然変異
+	static double rate[] = new double[parent];// good parent has high rate
+	// static int routeNum = 0;
+	// final static int mutation = 10; // the number of mutation突然変異
 
 	public static void main(String[] args) {
-		readFile("input_4.csv");
-		makeMultipleRoutes(routes);//create 500 random routes
-		allRoutesDistance(routes);/* 3. calculate each total distance*/
-		sortRoute(allDistances, 0, M - 1);/*4. sort route*/
+		readFile("input_2.csv");
+		makeMultipleRoutes(routes);// create random routes
+		multipleTwoOpt(routes);//sort
+		allRoutesDistance(routes);/* 3. calculate each total distance */
+		sortRoute(allDistances, 0, M - 1);/* 4. sort route */
 
-		System.out.println("10000th Generations's best route");
-		howGenerations(10000);
+		System.out.println("1000th Generations's best route");
+		howGenerations(1000);
 
 		for (int i = 0; i < routes[0].length; i++) {
-			System.out.print(routes[0][i] + " ");//elite
+			System.out.print(routes[0][i] + " ");// elite
 		}
 		System.out.println("");
 		System.out.println("best distance : " + allDistances[0]);
 		for (int i = 0; i < routes[0].length; i++) {
-			bestRoute[i] = routes[0][i];//elite
+			bestRoute[i] = routes[0][i];// elite
 		}
-		writeFile("output_4.csv", bestRoute);
+		//TwoOpt(bestRoute);
+		writeFile("output_2.csv", bestRoute);
 	}
 
 	/* 1. read lines from csv file and store the coordinates(x, y) */
@@ -77,6 +79,7 @@ public class TSP_GA {
 			e.printStackTrace();
 		}
 	}
+
 	/* this method make random route */
 	public static void makeRoute(int route[]) {
 		/* make array according to turn */
@@ -100,7 +103,6 @@ public class TSP_GA {
 		}
 	}
 
-	
 	/* this method return the distance between the two points */
 	public static double calculateDistance(int start, int goal) {
 		double distance = Math.pow(coordinates[goal][0] - coordinates[start][0], 2)
@@ -126,7 +128,7 @@ public class TSP_GA {
 		}
 	}
 
-	/* 4. sort random routes in ascending order*/
+	/* 4. sort random routes in ascending order */
 	static void sortRoute(double distances[], int left, int right) {
 		if (left >= right) {
 			return;
@@ -147,7 +149,7 @@ public class TSP_GA {
 				tmp = distances[l];
 				distances[l] = distances[r];
 				distances[r] = tmp;
-				for (int i = 0; i < routes[i].length; i++) {
+				for (int i = 0; i < routes[0].length; i++) {
 					pivot = routes[l][i];
 					routes[l][i] = routes[r][i];
 					routes[r][i] = pivot;
@@ -162,31 +164,96 @@ public class TSP_GA {
 
 	/* 5. choice parent route */
 	public static void parentRoutes() {
-		for (int i = 0; i < parentRoutes.length; i++) {
-			for (int j = 0; j < parentRoutes[i].length; j++) {
-				parentRoutes[i][j] = routes[i][j];
-				rate[i] = 10000/sumDistance(parentRoutes[i]);
-			}
+		int[] choosedParent = new int[parentRoutes.length];// next parents
+		double[] rateOfAll = new double[allDistances.length];
+		for (int i = 0; i < allDistances.length; i++) {
+			rateOfAll[i] = (allDistances.length - i) * (10 / allDistances[i]);
+			// System.out.print(rateOfAll[i]+" ");
 		}
+		// System.out.println("");
+		choosedParent[0] = 0;// elite
+		for (int i = 1; i < parentRoutes.length; i++) {
+			choosedParent[i] = rouletteWheelSelection(rateOfAll);
+			// System.out.print(choosedParent[i]+" ");
+		}
+		// System.out.println("");
+		int index = 0;
+		for (int i : choosedParent) {
+			for (int j = 0; j < parentRoutes[0].length; j++) {
+				parentRoutes[index][j] = routes[i][j];
+			}
+			rate[index] = (parentRoutes.length - index) * (100 / sumDistance(parentRoutes[index]));
+			// System.out.print(rate[index]+" ");
+			index++;
+		}
+		// System.out.println("");
 	}
-
-	/* 6. mutation by the rate of 5%(copy parent route into choosed routes & swap its order randomly) */
+	public static int[][] multipleTwoOpt(int route[][]){
+		for(int i=0; i<route.length; i++){
+			route[i] = TwoOpt(route[i]);
+		}
+		return route;
+	}
+	/*2-opt*/
+	public static int[] TwoOpt(int route[]){
+		int [] newRoute = new int[route.length];
+		double preDistance = sumDistance(route);
+	    // repeat until no improvement is made 
+	    int visited = 0;
+	    while ( visited<1 ){
+	        for ( int i = 1; i < N - 1; i++ )  {
+	        	for ( int k = 1; k < N; k++) {
+	        		//System.out.println(i+", "+k);
+	        		newRoute=TwoOptSort(route, i, k );
+	        		double newDistance = sumDistance(newRoute);
+	        		if (newDistance < preDistance) {
+	        			//System.out.println("sorted from "+i+" to "+k);
+	        			route = newRoute;
+	        		}else{
+		        		//route=TwoOptSort(route, i, k );
+	                }
+	            }
+	        }
+	        visited++;
+	    }
+	    return route;
+	}
+	//reverse order sort
+	public static int[] TwoOptSort(int[]route, int i, int j ) {
+		for (int k = 0; k < (j-i)/2; k++) {
+            int temp = route[j-k]; 
+            route[j-k]=route[i+k];
+            route[i+k]=temp;
+        }
+		return route;
+	}
+	/*
+	 * 6. mutation by the rate of 5%(copy parent route into choosed routes &
+	 * swap its order randomly)
+	 */
 	public static void mutantRoute() {
 		int pivot = 0;
-		Vector<Integer>choosedRoute = chooseFactors(parent, 0.05);
+		Vector<Integer> choosedRoute = chooseFactors(parent, 0.005, 0);// choose
+																		// factors
+																		// from
+																		// parent
+																		// by
+																		// the
+																		// rate
+																		// of
+																		// 0.5%
+																		// (skip
+																		// routes[0](elite))
 		/* copy parent route into choosed routes */
-		for (int i : choosedRoute) {
-			for (int j = 0; j < routes[i].length; j++) {
-				if(i != 0){
-					routes[i][j] = parentRoutes[i][j];
-				}
-			}
-		}
-		/* swap order randomly to routes[1]~[10] */
+		/*
+		 * for (int i : choosedRoute) { for (int j = 0; j < routes[i].length;
+		 * j++) { routes[i][j] = parentRoutes[i][j]; } }
+		 */
+		/* swap order randomly to choosed routes */
 		for (int i : choosedRoute) {
 			int rnd1 = (int) (Math.random() * (double) routes[i].length);
 			int rnd2 = (int) (Math.random() * (double) routes[i].length);
-			/*swap*/
+			/* swap */
 			pivot = routes[i][rnd1];
 			routes[i][rnd1] = routes[i][rnd2];
 			routes[i][rnd2] = pivot;
@@ -196,123 +263,133 @@ public class TSP_GA {
 	/* this method combine parent routes */
 	public static int[] combine(int route[]) {
 		int[][] parentRoutes2 = parentRoutes;
+		int[][] proposedRoutes = new int[2][route.length];
 		int rnd1 = (int) rouletteWheelSelection(rate);
 		int rnd2 = (int) rouletteWheelSelection(rate);
 		int start = (int) (Math.random() * (double) parentRoutes2[0].length);
 		int goal = (int) (Math.random() * (double) parentRoutes2[0].length);
 		int pivot = 0;
-		int flag = 0;
-		int index1 = 0;
-		int index2 = 0;
-		//System.out.println(rnd1+" "+rnd2);
-		if (start > goal) {//always start < goal
+		if (start > goal) {// always start < goal
 			pivot = start;
 			start = goal;
 			goal = pivot;
 		}
-		int index3 = goal + 1;
-		for (int i = 0; i < route.length; i++) {
-			route[i] = -1;
-		}
-		//select one parent route randomly & change a part of route into the parent route
-		for (int i = start; i < goal + 1; i++) {
-			route[i] = parentRoutes2[rnd1][i];
-		}
-
-		while (index1 < start) {//part before start (the changed part)
-			flag = 0;
+		/*try crossover for 2 pattern*/
+		for (int test = 0; test < 2; test++) {
+			int flag = 0;
+			int index1 = 0;
+			int index2 = 0;
+			int index3 = goal + 1;
 			for (int i = 0; i < route.length; i++) {
-				if (parentRoutes2[rnd2][index2] == route[i]) {
-					flag = 1;
-				}
+				proposedRoutes[test][i] = -1;
 			}
-			//if randomly selected city doesn't exist in route[], copy it into route(the part before start)
-			if (flag == 0) {
-				route[index1] = parentRoutes2[rnd2][index2];
-				index1++;
+			// select one parent route randomly & change a part of route into
+			// the parent route
+			for (int i = start; i < goal + 1; i++) {
+				proposedRoutes[test][i] = parentRoutes2[rnd1][i];
 			}
-			index2++;
-		}
 
-		while (index3 < route.length) {//part after goal (the changed part)
-			flag = 0;
-			for (int i = 0; i < route.length; i++) {
-				if (parentRoutes[rnd2][index2] == route[i]) {
-					flag = 1;
+			while (index1 < start) {// part before start (the changed part)
+				flag = 0;
+				for (int i = 0; i < route.length; i++) {
+					if (parentRoutes2[rnd2][index2] == proposedRoutes[test][i]) {
+						flag = 1;
+					}
 				}
+				// if randomly selected city doesn't exist in route[], copy it
+				// into route(the part before start)
+				if (flag == 0) {
+					proposedRoutes[test][index1] = parentRoutes2[rnd2][index2];
+					index1++;
+				}
+				index2++;
 			}
-			//if randomly selected city doesn't exist in route[], copy it into route(the part after goal)
-			if (flag == 0) {
-				route[index3] = parentRoutes[rnd2][index2];
-				index3++;
+
+			while (index3 < route.length) {// part after goal (the changed part)
+				flag = 0;
+				for (int i = 0; i < route.length; i++) {
+					if (parentRoutes[rnd2][index2] == proposedRoutes[test][i]) {
+						flag = 1;
+					}
+				}
+				// if randomly selected city doesn't exist in route[], copy it
+				// into route(the part after goal)
+				if (flag == 0) {
+					proposedRoutes[test][index3] = parentRoutes[rnd2][index2];
+					index3++;
+				}
+				index2++;
 			}
-			index2++;
 		}
-		return route;
+		/*select better route than the other*/
+		if(sumDistance(proposedRoutes[0])<sumDistance(proposedRoutes[1])){
+			return proposedRoutes[0];
+		}else{
+			return proposedRoutes[1];
+		}
 	}
 
-	/*this method is roulette wheel selection*/
-	public static int rouletteWheelSelection(double[] rate){
+	/* this method is roulette wheel selection */
+	public static int rouletteWheelSelection(double[] rate) {
 		Random rnd = new Random();
 		double sum = 0;
-		for(int i = 0; i < rate.length; i++ ){
+		for (int i = 0; i < rate.length; i++) {
 			sum += rate[i];
 		}
-		double temp = rnd.nextDouble() * sum ;
+		double temp = rnd.nextDouble() * sum;
 		/* P(i)=(sum-rate[i])/sum */
-		for(int i = 0;i < rate.length ;i++ ){
+		for (int i = 0; i < rate.length; i++) {
 			temp -= rate[i];
-			if(temp < 0){
-				return i;	
+			if (temp < 0) {
+				return i;
 			}
 		}
 		return -1;
 	}
-	
-	/*this method choose factors in specific proportions*/
-	public static Vector<Integer> chooseFactors(int arrayLength, double rate){
-		int maximumValue = (int)(rate*arrayLength);
+
+	/* this method choose factors in specific proportions */
+	public static Vector<Integer> chooseFactors(int arrayLength, double rate, int ban) {
+		int maximumValue = (int) (rate * arrayLength);
 		int factorsNum = 0;
-		Vector <Integer>choosedFactors = new Vector<Integer>(maximumValue);
-		while(factorsNum<maximumValue){
+		Vector<Integer> choosedFactors = new Vector<Integer>(maximumValue);
+		while (factorsNum < maximumValue) {
 			int random = (int) (Math.random() * arrayLength);
-			if(!choosedFactors.contains(random)){
+			if (!choosedFactors.contains(random) && random != ban) {
 				choosedFactors.add(random);
 				factorsNum++;
 			}
 		}
 		return choosedFactors;
 	}
-	
-	/* 7. make multiple combined parent routes by the rate of 80%*/
+
+	/* 7. make multiple combined parent routes by the rate of 85% */
 	public static void multipleCombine() {
-		Vector<Integer>cannotChoose = chooseFactors(parent, 0.2);
-		for (int i = 1; i < routes.length; i++) {
-			if(!cannotChoose.contains(i)){
-				routes[i]=combine(routes[i]);
-			}
+		Vector<Integer> choosedRoutes = chooseFactors(parent, 0.85, 0);
+		for (int i : choosedRoutes) {
+			routes[i] = combine(routes[i]);
 		}
 	}
 
 	/* 8. make next generations */
 	public static void nextGeneration() {
 		parentRoutes();
-		multipleCombine();//crossing
-		mutantRoute();//mutation
+		//multipleTwoOpt(parentRoutes);
+		multipleCombine();// crossing
+		mutantRoute();// mutation
 		allRoutesDistance(routes);
 		sortRoute(allDistances, 0, M - 1);
 	}
 
 	/* 9. designate number of generations */
 	public static void howGenerations(int number) {
-		double distance = 0;//for check
+		double distance = 0;// for check
 		for (int i = 0; i < number; i++) {
 			nextGeneration();
-			//for check
-			if(distance != allDistances[0]){
-				System.out.println("updated! "+allDistances[0]+" ("+i+")");
+			// for check
+			if (distance != allDistances[0]) {
+				System.out.println("updated! " + allDistances[0] + " (" + i + ")");
 			}
-			distance =allDistances[0];
+			distance = allDistances[0];
 		}
 	}
 
